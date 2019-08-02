6ccfcdc5c95d76d794160287f0a241f5b079ba5f
Annabella FILLETTAZ <annabellafillettaz@MacBook-Pro-de-Annabella.local>
2019-08-02 12:48:54 +0200

    Comment Error Managed

--- src/helpers/http.ts
+++ src/helpers/http.ts
@@ -23,17 +23,30 @@ const httpError = ( status: number, msg = '' ) => ( message = msg, err = undefin
 //   return httpErrorFunction;
 //
 // };
-export const httpError500 = httpError( 500, 'Internal Server Error' );
 
+/*******************************************************************
+*******  BAD REQUEST                                         *******
+********************************************************************/
 export const httpError400 = httpError( 400 );
-
+/*******************************************************************
+*******  UNAUTHORIZED                                        *******
+********************************************************************/
 export const httpError401 = httpError( 401 );
-
+/*******************************************************************
+*******  FORBIDDEN                                           *******
+********************************************************************/
 export const httpError403 = httpError( 403 );
-
+/*******************************************************************
+*******  NOT FOUND                                           *******
+********************************************************************/
 export const httpError404 = httpError( 404 );
-
-// MANAGE ERRORS MONGO
+/*******************************************************************
+*******  ERRORS NOT MANAGED                                  *******
+********************************************************************/
+export const httpError500 = httpError( 500, 'Internal Server Error' );
+/*******************************************************************
+*******  ERRORS MONGO MANAGED                                *******
+********************************************************************/
 export const mongoError = ( err, res: Response ) => {
   if ( err instanceof Error.ValidationError )
     res.status( 400 ).send( httpError400( 'Validation error', err ) );
